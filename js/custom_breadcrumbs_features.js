d19d9df1f5affb8081ef6c3ef6e2e6f885ef2330
Fengtan <Fengtan@847318.no-reply.drupal.org>
2013-10-14 16:39:37 -0400

    by Fengtan: Added JS.

---
+++ js/custom_breadcrumbs_features.js
@@ -0,0 +1,54 @@
+(function ($) {
+
+Drupal.behaviors.customBreadcrumbsFeatures = {
+  attach: function (context) {
+
+    // Declare elements used to generate cb name.
+    var sources = {
+      'custom-breadcrumbs-form':                     '#edit-node-type',
+      'custom-breadcrumbsapi-form':                  '#edit-module-page',
+      'custom-breadcrumbs-paths-form':               '#edit-specific-path',
+      'custom-breadcrumbs-panels-form':              '#edit-panel-id',
+      'custom-breadcrumbs-taxonomy-term-form':       '#edit-tid',
+      'custom-breadcrumbs-taxonomy-vocabulary-form': '#edit-vid',
+      'custom-breadcrumbs-views-form':               '#edit-views-path'
+    };
+
+    //TODO test
+    var generated = !$('#edit-name', context).val();
+
+    // Find out which element we should use.
+    $.each(sources, function(form, source) {
+      if (context['forms'][form] != null) {
+
+        // Add event handler so the name is updated when we change the source.
+        $(source, context).change(function() {
+          var val = '';
+          switch ($(source)[0].tagName.toLowerCase()) {
+            case 'select':
+              val = $(source + ' option:selected').text();
+              break;
+            default:
+              val = $(source).val();
+          }
+          var name = $('#edit-name', context);
+          if (!name.val() || generated) {
+            name.val(val);
+            name.change();
+          }
+        });
+
+        // Assume that if the user edited the name, we do not need to generate one.
+        $('#edit-name', context).focus(function() {
+          generated = false;
+        });
+
+        // No need to loop over all elements.
+        return false;
+
+      }
+    });
+  }
+};
+
+})(jQuery);
